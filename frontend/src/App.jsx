a320da6a0d4287f93b288269824f9fa925edfbfa
Arpit <arpitjoshi213.com>
2025-10-23 21:13:30 +0530

    added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Addevent from "./pages/Addevent";
+import Eventpage from "./pages/Eventpage";
+import Home from "./pages/Home";
+import Pevent from "./pages/Pevent";
+import Register from "./pages/Register";
 
 function App() {
- 
-
   return (
-    <>
-     <p  className="bg-red-400">hlo</p>
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Eventpage />} />
+        <Route path="/pevent" element={<Pevent />} />
+        <Route path="/addevent" element={<Addevent />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='flex px-12 py-6 bg-gray-100 justify-between'>
+        <div>
+            CampusVibes
+        </div>
+        <div >
+<ul className='flex gap-8'>
+            <li>Home</li>
+            <li>Events</li>
+        </ul>
+        </div>
+        
+    </div>
   )
 }
